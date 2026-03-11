fe67a2de9f9c885e2c530efb06c42434db036419
Alien <alienattitude@gmail.com>
2026-03-11 06:24:28 +0100

    few changes

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { Navbar} from "../components/Navbar.tsx";
+import { Navbar} from "../components/Navbar";
 import {
     UserPlusIcon,
     SearchIcon,
